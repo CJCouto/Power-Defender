945a7338e38d1392e715340b9790bfedb9390d63
CJCouto <36166895+CJCouto@users.noreply.github.com>
2018-02-05 15:59:25 -0500

    Add files via upload

---
+++ EnemyDeath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour {
+
+	public int Health = 2;
+	public float DelayDeath;
+
+	void start() 
+	{
+		Health = 2;
+	}
+
+	//Reduces health on hit
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player Projectile")) 
+		{
+			Health -= 1;
+		}
+
+		//Destroys enemy with an optional delay
+		if (Health <= 0) 
+		{
+			Destroy (gameObject, DelayDeath);
+		}
+	}
+}
